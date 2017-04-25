13a0020cb4942e04eb8b03b18bf769abeb9ca001
Josh Hamilton <nearbycoder@gmail.com>
2017-04-25 14:01:25 -0700

    add alias support for markup-inline-loader

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const PATTERN = /<(svg|img|math)\s+(.*?)src\s*=\s*"(.*?)"(.*?)\/?>/gi;
 const fs = require('fs');
 const path = require('path');
 const SVGO = require('svgo');
-const loaderUtils = require('loader-utils');
+const asyncReplace = require('async-replace');
 
 const SVGOConfiguration = {
   plugins: [
@@ -16,31 +16,41 @@ const SVGOConfiguration = {
 module.exports = function (content) {
   this.cacheable && this.cacheable();
   const loader = this;
+  const callback = this.async();
+  const loaderUtils = require('loader-utils');
   const options = Object.assign({strict: '[markup-inline]'}, loaderUtils.getOptions(this));
   const svgo = new SVGO(options.svgo || SVGOConfiguration);
   const strict = options.strict.replace(/\[(data-)?([\w-]+)\]/, '$2');
 
-  content = content.replace(PATTERN, replacer);
-  return content;
-
-  function replacer(matched, tagName, preAttributes, fileName, postAttributes) {
+  function replacer(match, tagName, preAttributes, fileName, postAttributes, offset, string, done) {
     const isSvgFile = path.extname(fileName).toLowerCase() === '.svg';
     const isImg = tagName.toLowerCase() === 'img';
-    const meetStrict = !strict || new RegExp(`[^\w-](data-)?${strict}[^\w-]`).test(matched);
+    const meetStrict = !strict || new RegExp(`[^\w-](data-)?${strict}[^\w-]`).test(match);
 
     if (isImg && !isSvgFile || !meetStrict) {
-      return matched;
-    }
+      done(null, match);
+    } else {
 
-    const filePath = loaderUtils.urlToRequest(path.join(loader.context, fileName), '/');
-    loader.addDependency(filePath);
-    let fileContent = fs.readFileSync(filePath, {encoding: 'utf-8'});
-    if (isSvgFile) {
-      // It's callback, But it's sync call, So, we needn't use async loader
-      svgo.optimize(fileContent, (result) => {
-        fileContent = result.data;
+      const filePath = loaderUtils.urlToRequest(fileName, options.root);
+      
+      loader.resolve(loader.context, filePath, function(err, resolvedFilePath) {
+        if (err) done(err);
+        loader.addDependency(resolvedFilePath);
+        let fileContent = fs.readFileSync(resolvedFilePath, {encoding: 'utf-8'});
+        if (isSvgFile) {
+          // It's callback, But it's sync call, So, we needn't use async loader
+          svgo.optimize(fileContent, (result) => {
+            fileContent = result.data;
+          });
+        }
+        let replacedContent = fileContent.replace(/^<svg/, '<svg ' + preAttributes + postAttributes + ' ');
+
+        done(null, replacedContent);
       });
     }
-    return fileContent.replace(/^<(svg|math)/, '<$1 ' + preAttributes + postAttributes + ' ');
   }
+
+  asyncReplace(content, PATTERN, replacer, function(err, result) {
+    callback(err, result);
+  });
 };
